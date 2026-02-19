8db371156bbc26556ce6b52da00deee3f24c4e69
TimothyMuigai <timothy.muigai@strathmore.edu>
2026-02-19 03:21:54 +0300

    finished what i know

--- src/components/layout/companyCulture.tsx
+++ src/components/layout/companyCulture.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { motion, Variants  } from 'framer-motion';
 import Image from 'next/image';
 import {
     ShieldCheck,
@@ -8,7 +8,6 @@ import {
     Newspaper,
     EyeOff,
     UserCheck,
-    ArrowRight,
 } from 'lucide-react';
 
 const leadership = [
@@ -98,14 +97,18 @@ const containerVariants = {
     },
 };
 
-const cardVariants = {
+const cardVariants: Variants = {
     hidden: { opacity: 0, y: 40 },
     show: {
         opacity: 1,
         y: 0,
-        transition: { duration: 0.6, ease: "easeOut" },
+        transition: {
+            duration: 0.6,
+            ease: "easeOut",
+        },
     },
 };
+
 
 export default function CompanyCulture() {
     return (
