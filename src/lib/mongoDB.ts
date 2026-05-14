10551031a1f2e936f3681cd8dd59ffe6a86d5fac
Ian-Ngari <ianngari02@gmail.com>
2026-05-14 04:52:51 +0300

    vercel error fixed

--- src/lib/mongoDB.ts
+++ src/lib/mongoDB.ts
@@ -12,20 +12,19 @@ declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient> | undefined;
+let clientPromise: Promise<MongoClient>;
 
-if (uri) {
-  if (process.env.NODE_ENV === "development") {
-    if (!global._mongoClientPromise) {
-      client = new MongoClient(uri, options);
-      global._mongoClientPromise = client.connect();
-    }
-    clientPromise = global._mongoClientPromise;
-  } else {
-    client = new MongoClient(uri, options);
-    clientPromise = client.connect();
+if (!uri) {
+  clientPromise = Promise.reject(new Error("Missing MONGODB_URI environment variable"));
+} else if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    const client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
   }
+  clientPromise = global._mongoClientPromise;
+} else {
+  const client = new MongoClient(uri, options);
+  clientPromise = client.connect();
 }
 
 export default clientPromise;
