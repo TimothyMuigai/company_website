ab1510044c43df6f91f305b5b5273e978458e621
deeptrck <ianngari02@gmail.com>
2026-06-29 09:46:37 +0300

    fix: remove clerk middleware for EC2 deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,4 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
-
+export function middleware() {}
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: [],
 };
